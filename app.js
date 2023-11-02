51e2829e679ad784304b5f570ec621ba6514d6b2
sathvikrdy09 <cherukusathvikreddy@gmail.com>
2023-11-02 00:10:19 -0500

    added app.js and bin/www and views/index.pug and views/layout.pug and public/images/ and routes/board.js and routes/choose.js and routes/juice.js and views/board.pug and views/choose.pug and views/juice.pug

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var juiceRouter = require('./routes/juice');
+var boardRouter = require('./routes/board');
+var chooseRouter = require('./routes/choose');
+
 
 var app = express();
 
@@ -21,6 +25,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/juice', juiceRouter);
+app.use('/board', boardRouter);
+app.use('/choose', chooseRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
